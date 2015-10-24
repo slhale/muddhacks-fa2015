426366aaa9461b6105588526ea0ec4abd4e7fd17
mknittel <mknittel@g.hmc.edu>
2015-10-24 04:31:17 -0700

    Uncommenting one slide

--- counterclock/counterclock.ino
+++ counterclock/counterclock.ino
@@ -218,8 +218,8 @@ void drawStems() {
   }
 
   if (mode == "slide" && stemsIterations % 100 == 0) {
-    /*writeStems(xOneStart, 0, 0, true);
-    writeStems(xTwoStart, 12, 0, true);
+    writeStems(xOneStart, 0, 0, true);
+    /*writeStems(xTwoStart, 12, 0, true);
     writeStems(xThreeStart, 24, 0, true);*/
 
     if (xOneStart < 1) {
